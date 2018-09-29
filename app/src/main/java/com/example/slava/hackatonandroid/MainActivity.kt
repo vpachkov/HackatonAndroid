5f18cbfc0f850e189a4819ce64fc63e84f85b7fd
Vakosta <zzoorm@gmail.com>
2018-09-29 11:12:46 +0300

    Update Kotlin version

--- app/src/main/java/com/example/slava/hackatonandroid/MainActivity.kt
+++ app/src/main/java/com/example/slava/hackatonandroid/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.example.slava.hackatonandroid
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
