bc3c816345f34056b62f3f3312c9df82b8ff09cf
Vakosta <zzoorm@gmail.com>
2018-09-29 14:35:48 +0300

    Create discover place activity

---
+++ app/src/main/java/com/example/slava/hackatonandroid/presentation/DiscoverPlaceActivity.kt
@@ -0,0 +1,34 @@
+package com.example.slava.hackatonandroid.presentation
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.slava.hackatonandroid.R
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+
+class DiscoverPlaceActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dicover_place)
+
+        (supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment).getMapAsync(this)
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        mMap = map
+        setupMap()
+    }
+
+    private fun setupMap() {
+        mMap.addMarker(MarkerOptions().position(LatLng(59.146361, 47.267305))
+                .title("Lolkek").icon(BitmapDescriptorFactory
+                        .defaultMarker(BitmapDescriptorFactory.HUE_RED)))
+    }
+}
