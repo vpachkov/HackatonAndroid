5ab8ebec7f975be0c4aff3f882fecf6ecbb1a320
Vakosta <zzoorm@gmail.com>
2018-09-29 16:00:21 +0300

    Layout is amazing

--- app/src/main/java/com/example/slava/hackatonandroid/presentation/DiscoverPlaceActivity.kt
+++ app/src/main/java/com/example/slava/hackatonandroid/presentation/DiscoverPlaceActivity.kt
@@ -3,12 +3,14 @@ package com.example.slava.hackatonandroid.presentation
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.example.slava.hackatonandroid.R
+import com.example.slava.hackatonandroid.domain.utils.TextAdder
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_dicover_place.*
 
 
 class DiscoverPlaceActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -19,6 +21,8 @@ class DiscoverPlaceActivity : AppCompatActivity(), OnMapReadyCallback {
         setContentView(R.layout.activity_dicover_place)
 
         (supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment).getMapAsync(this)
+
+        parentLayout.addView(TextAdder.makeHatBlock("Discover", "The place", this), 0)
     }
 
     override fun onMapReady(map: GoogleMap) {
