04bf55b760cb7d8dc993b380bb6b3a75b93c3bbf
Vakosta <zzoorm@gmail.com>
2018-09-29 14:47:25 +0300

    Add permissions

--- app/src/main/java/com/example/slava/hackatonandroid/presentation/MainActivity.kt
+++ app/src/main/java/com/example/slava/hackatonandroid/presentation/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.example.slava.hackatonandroid.presentation
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.example.slava.hackatonandroid.R
-import com.example.slava.hackatonandroid.data.PreferencesHelper
 import com.example.slava.hackatonandroid.domain.utils.TextAdder
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,13 +13,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //if user hasn't been authorized move him to authorization
+        startActivity(Intent(this, DiscoverPlaceActivity::class.java))
+
+        /*//if user hasn't been authorized move him to authorization
         if (!(PreferencesHelper.getSharedPreferenceBoolean(applicationContext, PreferencesHelper.KEY_IS_LOGINED, false))) {
             startActivity(Intent(this, LoginActivity::class.java))
-        }
+        }*/
 
         main_activity_main_block.addView(
-                TextAdder.makeHatBlock("Discover new " , "Places", this)
+                TextAdder.makeHatBlock("Discover new ", "Places", this)
         )
     }
 }
