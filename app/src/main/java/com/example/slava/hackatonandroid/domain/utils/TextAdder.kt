a8fc5320abdb9925628c95a70ab0a128325e36ed
Vakosta <zzoorm@gmail.com>
2018-09-30 12:57:17 +0300

    Add layout generator

--- app/src/main/java/com/example/slava/hackatonandroid/domain/utils/TextAdder.kt
+++ app/src/main/java/com/example/slava/hackatonandroid/domain/utils/TextAdder.kt
@@ -2,8 +2,8 @@ package com.example.slava.hackatonandroid.domain.utils
 
 import android.content.Context
 import android.graphics.Typeface
-import android.graphics.drawable.Drawable
 import android.util.TypedValue
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -11,25 +11,25 @@ import com.example.slava.hackatonandroid.R
 
 object TextAdder {
 
-    fun makeHatBlock(topLineText: String , bottomLineText: String , context: Context): LinearLayout{
+    fun makeHatBlock(topLineText: String, bottomLineText: String, context: Context): LinearLayout {
 
         val hatBlock = LinearLayout(context)
         val topLine = TextView(context)
         val bottomline = TextView(context)
         val gradientView = View(context)
 
-        val hatBlockParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT , LinearLayout.LayoutParams.WRAP_CONTENT)
-        val topLineParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT , LinearLayout.LayoutParams.WRAP_CONTENT)
-        val bottomLineParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT , LinearLayout.LayoutParams.WRAP_CONTENT)
-        val gradientViewParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT , SizeConverter.convertDpToPixels(7f , context))
+        val hatBlockParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+        val topLineParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+        val bottomLineParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+        val gradientViewParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, SizeConverter.convertDpToPixels(7f, context))
 
         topLineParams.setMargins(
-                SizeConverter.convertDpToPixels(10f , context) , SizeConverter.convertDpToPixels(20f , context) , 0 , 0)
+                SizeConverter.convertDpToPixels(10f, context), SizeConverter.convertDpToPixels(20f, context), 0, 0)
 
         bottomLineParams.setMargins(
-                SizeConverter.convertDpToPixels(10f , context) , SizeConverter.convertDpToPixels(20f , context) , 0 , 0)
+                SizeConverter.convertDpToPixels(10f, context), SizeConverter.convertDpToPixels(20f, context), 0, 0)
 
-        gradientViewParams.setMargins( 0 , SizeConverter.convertDpToPixels(20f , context) , 0 , 0)
+        gradientViewParams.setMargins(0, SizeConverter.convertDpToPixels(20f, context), 0, 0)
 
 
         topLine.layoutParams = topLineParams
@@ -39,13 +39,13 @@ object TextAdder {
         topLine.text = topLineText
         bottomline.text = bottomLineText
 
-        topLine.setTextSize(TypedValue.COMPLEX_UNIT_SP , 30f)
-        bottomline.setTextSize(TypedValue.COMPLEX_UNIT_SP , 35f)
+        topLine.setTextSize(TypedValue.COMPLEX_UNIT_SP, 30f)
+        bottomline.setTextSize(TypedValue.COMPLEX_UNIT_SP, 35f)
 
         topLine.setTextColor(context.resources.getColor(R.color.colorGray))
         bottomline.setTextColor(context.resources.getColor(R.color.colorGray))
 
-        bottomline.setTypeface(null , Typeface.BOLD)
+        bottomline.setTypeface(null, Typeface.BOLD)
 
         gradientView.setBackgroundDrawable(context.getDrawable(R.drawable.horizontal_gradient))
 
@@ -56,5 +56,22 @@ object TextAdder {
         hatBlock.addView(gradientView)
 
         return hatBlock
+    }
+
+    fun makeItemPlaces(context: Context, firstText: String, secondText: String): View {
+        val params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT)
+        val view = LayoutInflater.from(context).inflate(R.layout.item_places, null, false)
+
+        params.marginStart = SizeConverter.convertDpToPixels(8f, context)
+        params.marginEnd = SizeConverter.convertDpToPixels(8f, context)
+        params.bottomMargin = SizeConverter.convertDpToPixels(8f, context)
+
+        view.layoutParams = params
+
+        view.findViewById<TextView>(R.id.firstText).text = firstText
+        view.findViewById<TextView>(R.id.secondText).text = secondText
+
+        return view
     }
 }
