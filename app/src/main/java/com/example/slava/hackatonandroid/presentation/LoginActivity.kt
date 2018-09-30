45d48ce4544c5006dee88238847899e579831582
Plunkerusr <slava.pach@gmail.com>
2018-09-30 12:08:48 +0300

    add anko and khttp (for Nikita)

--- app/src/main/java/com/example/slava/hackatonandroid/presentation/LoginActivity.kt
+++ app/src/main/java/com/example/slava/hackatonandroid/presentation/LoginActivity.kt
@@ -3,9 +3,11 @@ package com.example.slava.hackatonandroid.presentation
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import com.example.slava.hackatonandroid.R
 import com.example.slava.hackatonandroid.domain.utils.TextAdder
 import kotlinx.android.synthetic.main.activity_login.*
+import khttp.post
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +19,19 @@ class LoginActivity : AppCompatActivity() {
 
         register_button.setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
+        login_activity_login_button.setOnClickListener {
+            val loginString = login_activity_login.text.toString()
+            val passwordString = login_activity_password.text.toString()
+
+            val params = mapOf(
+                    "login" to loginString,
+                    "password" to passwordString
+            )
+
+            val responseLogin = post("https://https://dookyheroky.herokuapp.com/admin/api/user/addnew/" , params = params)
+            Log.e("kekek" , responseLogin.text.toString())
         }
     }
 }
